6739808b6b347818d6a1e15ae04e0015091ab80f
joao <joao.andrade@petlove.com.br>
2023-07-03 22:54:25 -0300

    rowback to preview works

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const routes = [
 function App() {
 
   return (
-    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/julie_store/'}>
+    <BrowserRouter>
       <UserStorage>
         <Routes>
           <Route path="/" element={<Login />} />
