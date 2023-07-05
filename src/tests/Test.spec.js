60edaae8cc1e2af4ee96ad819fbf6119b226a689
joao <joao.andrade@petlove.com.br>
2023-07-05 14:55:00 -0300

    fest: add jest and rtl to project

---
+++ src/tests/Test.spec.js
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Test from "../pages/Test";
+
+describe("Test", () => {
+    it('should render button enabled', async () => {
+        render(<Test client={{ id: 1 }} />);
+
+        const button = screen.getByRole('button', { name: 'Toogle Edit' })
+
+        expect(button).toBeEnabled()
+    })
+
+    it('should render the correct header based on isEdit state', () => {
+        render(<Test />);
+
+        expect(screen.queryByText('Create')).toBeInTheDocument();
+        expect(screen.queryByText('Edit')).toBeNull();
+
+        // Clicar no botão "Toogle Edit"
+        fireEvent.click(screen.getByRole('button', { name: 'Toogle Edit' }));
+
+        expect(screen.queryByText('Edit')).toBeInTheDocument();
+        expect(screen.queryByText('Create')).toBeNull();
+    })
+
+    it('should update the name state when typing in the input', () => {
+        render(<Test />);
+
+        const input = screen.getByPlaceholderText('Name:');
+
+        // Simular a digitação de um nome
+        fireEvent.change(input, { target: { value: 'John Doe' } });
+
+        expect(input.value).toBe('John Doe');
+    })
+})
+
